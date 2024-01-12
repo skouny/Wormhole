c848f66bfce6af367ec6b019b91634232c77b0f5
Yiannis Skounakis <skouny@gmail.com>
2024-01-12 15:50:10 +0200

    up

---
+++ SETUP.md
@@ -0,0 +1,19 @@
+# Project Setup
+
+## Firebase
+
+`npm install firebase`
+
+`npm install -g firebase-tools`
+
+`firebase login`
+
+`firebase init hosting`
+
+`firebase init firestore`
+
+`firebase init functions`
+
+## Firebase UI
+
+`npm install firebaseui`
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+/** */
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [
+    CommonModule,
+    RouterOutlet
+  ]
 })
 export class AppComponent {
   title = 'Wormhole';
