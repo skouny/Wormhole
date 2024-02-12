df0b639a30c2c5384a937305f676372d42e116f6
Yiannis Skounakis <skouny@gmail.com>
2024-02-12 15:04:02 +0200

    up

--- functions/src/toolbox/firebase.ts
+++ functions/src/toolbox/firebase.ts
@@ -1,14 +1,20 @@
 import { initializeApp } from "firebase-admin/app";
 import { firestore } from "firebase-admin";
 import { CollectionName, OpapDrawV3 } from "../../../shared/data-types";
-
 /** Initialize Firebase */
 initializeApp();
-
 /** Firestore instance & settings */
 export const db = firestore();
 db.settings({ ignoreUndefinedProperties: true });
-
-/** Available collections */
+/** */
 export const collectionRef = <T>(collectionName: CollectionName) => db.collection(collectionName) as firestore.CollectionReference<T>;
+/** */
+export const documentUID = <T>(snapshot: firestore.DocumentSnapshot<T> | firestore.QueryDocumentSnapshot<T>) => {
+  const data = snapshot.data();
+  if (!snapshot.exists) return;
+  if (data === undefined) return;
+  return { ...data, UID: snapshot.id };
+};
+// #region Available collections
 export const collection_OpapDrawV3 = collectionRef<OpapDrawV3>(CollectionName.OpapDrawV3);
+// #endregion
